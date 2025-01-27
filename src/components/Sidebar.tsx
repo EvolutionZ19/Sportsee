5f18a3f3e3df21632755800997005c1cfefb4c25
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2025-01-27 13:29:26 +0100

    Ajout de la sibebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li>🏋️‍♀️</li>
+        <li>🏊‍♂️</li>
+        <li>🚴‍♂️</li>
+        <li>💪</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
